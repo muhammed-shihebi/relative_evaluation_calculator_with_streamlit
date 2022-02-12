d5236c92a94aa96d9dab3b2e18af564318fda3f9
Muhammed Şihebi <58932402+Nour0700@users.noreply.github.com>
2022-02-12 16:06:24 +0300

    edit text

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import calculator as calc
 
 
 st.title("Relative evaluation calculator (Bağıl değerlendirme hesap makinesi)")
-my_grade = st.number_input('Your score (real or estimate)', min_value=0.0, max_value=100.0, value=90.0, step=1.0)
-m = st.number_input('Average score of your class (real or estimate)', min_value=0.0, max_value=100.0, value=50.0, step=1.0)
+my_grade = st.number_input('Your grade (real or estimate)', min_value=0.0, max_value=100.0, value=90.0, step=1.0)
+m = st.number_input('Average grade of your class (real or estimate)', min_value=0.0, max_value=100.0, value=50.0, step=1.0)
 s = st.number_input('Standard deviation (normally, it is near 15)', min_value=8.0, max_value=50.0, value=15.0, step=1.0)
 t = st.number_input('Tolerance level (0 by default = no tolerance)', min_value=0.0, max_value=0.2, value=0.0, step=0.1)
 
 
-st.metric(label="Your letter score", value=calc.cal_letter(my_grade, m, s, t))
+st.metric(label="Your letter grade", value=calc.cal_letter(my_grade, m, s, t))
 
 letters = calc.get_letters(m, s, t)
 
